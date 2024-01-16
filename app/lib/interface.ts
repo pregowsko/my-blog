8d9a0a45daea7cd995aaa18672133742ec8f7d4d
Aleksandra Stanek <aleksandrastanek@MacBook-Pro-Aleksandra.local>
2024-01-16 16:08:00 +0100

    „lib„

---
+++ app/lib/interface.ts
@@ -0,0 +1,6 @@
+export interface simpleBlogcard {
+    title: string;
+    smallDescription: string;
+    currentSlug: string;
+    titleImage: any;
+}
--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,24 @@
-async function getData() {
+import { simpleBlogcard } from "./lib/interface";
+import {client} from "./lib/sanity";
 
+async function getData() {
+  const query = `
+  *[_type == 'blog'] | order(_createdAt desc) {
+    title,
+      smallDescription,
+      "currentSlug": slug.current,
+      titleImage
+  }`;
+
+  const data = await client.fetch(query);
+
+  return data;
 }
 
 
-export default function Home() {
+export default async function Home() {
+  const data: simpleBlogcard[] = await getData();
+
   return (
     <div>
       <h1>hello form the index</h1>
