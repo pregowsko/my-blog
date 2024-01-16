9e4d5103d00fd1b6173d762cf0294d5a29af9e69
Aleksandra Stanek <aleksandrastanek@MacBook-Pro-Aleksandra.local>
2024-01-16 16:33:10 +0100

    „interface„

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,9 @@
+import { Card, CardContent } from "@/components/ui/card";
+import { Button } from "@/components/ui/button";
 import { simpleBlogcard } from "./lib/interface";
-import {client} from "./lib/sanity";
+import {client, urlFor} from "./lib/sanity";
+import Image from "next/image";
+import Link from "next/link";
 
 async function getData() {
   const query = `
@@ -20,8 +24,25 @@ export default async function Home() {
   const data: simpleBlogcard[] = await getData();
 
   return (
-    <div>
-      <h1>hello form the index</h1>
+    <div className="grid grid-cols-1 md:grid-cols-2 mt-5 gap-5">
+      {data.map((post, idx) =>(
+        <Card key={idx}>
+          <Image 
+          src={urlFor(post.titleImage).url()} 
+          alt="image" 
+          width={500} 
+          height={500} 
+          className="rounded-t-lg h-[200px] object-cover"
+          />
+          <CardContent className="mt-5">
+            <h3 className="text-lg line-clamp-2 font-bold">{post.title}</h3>
+            <p className="line-clamp-3 text-sm mt-2 text-gray-600 dark:text-gray-300">{post.smallDescription}</p>
+            <Button asChild className="w-full mt-7">
+              <Link href={`/blog/${post.currentSlug}`}>Read More</Link>
+            </Button>
+          </CardContent>
+        </Card>
+      ))}
     </div>
   )
 }
