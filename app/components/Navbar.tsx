d403d14304ae341fecf010d018a05405f18a6575
Aleksandra Stanek <aleksandrastanek@MacBook-Pro-Aleksandra.local>
2024-01-16 14:32:21 +0100

    „components„

--- app/components/Navbar.tsx
+++ app/components/Navbar.tsx
@@ -1,11 +1,14 @@
 import Link from "next/link";
+import { ModeToggle } from "./ModeToggle";
 
 export default function Navbar(){
     return(
     <nav className="w-full relative flex items-center justify-between max-w-2xl mx-auto py-5 px-4">
         <Link href="/" className="font-bold text-3xl">
-            Aleksandra<span className="text-green-500">Blog</span>
+            Aleksandra<span className="text-primary">Blog</span>
         </Link>
+
+        <ModeToggle/>
     </nav>
     );
 }
--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,11 @@
-import Image from 'next/image'
-import Navbar from './components/Navbar'
+async function getData() {
+
+}
+
 
 export default function Home() {
   return (
     <div>
-      <Navbar />
       <h1>hello form the index</h1>
     </div>
   )
